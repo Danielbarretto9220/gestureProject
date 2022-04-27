26db073a1cc5095ee495d2b84ede9228e8190fac
Danielbarretto9220 <danielbarretto.10@gmail.com>
2022-04-27 16:59:26 +0530

    trex mode

--- gestureGames/urls.py
+++ gestureGames/urls.py
@@ -6,7 +6,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home),
+    path('hHome/', views.home),
     path('trex/', views.trex, name='trex'),
     path('tictactoe/', views.tictactoe, name='tictactoe'),
     path('video_stream', views.video_stream, name='video_stream'),
+    path('video_stream2', views.video_stream2, name='video_stream2'),
 ]
--- gestureGames/views.py
+++ gestureGames/views.py
@@ -2,14 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http.response import StreamingHttpResponse
 from gestureGames.camera import VideoCamera
+from gestureProject.gestureGames.camera2 import VideoCamera2
 # Create your views here.
 
 def home(request):
     return render(request, 'Home.html')
 
+
+# TReX
 def trex(request):
     return render(request, 'trex.html')
 
+def gen2(camera2):
+    while True:
+        frame = camera2.get_frame()
+        yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+def video_stream2(request):
+    return StreamingHttpResponse(gen2(VideoCamera2()),
+                content_type = 'multipart/x-mixed-replace; boundary=frame')
+
+
+# Tic Tac Toe
 def tictactoe(request):
     return render(request, 'tictactoe.html')
 
@@ -21,3 +36,4 @@ def gen(camera):
 def video_stream(request):
     return StreamingHttpResponse(gen(VideoCamera()),
                     content_type='multipart/x-mixed-replace; boundary=frame')
+
